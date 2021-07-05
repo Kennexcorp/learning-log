a9613b47e342728cd510ccbd0eaa084a553f4293
Ekene Oguikpu <ekeneoguikpu@gmail.com>
2021-07-05 14:24:45 +0100

    associated users to their topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .models import Entry, Topic
 from .forms import EntryForm, TopicForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 # Create your views here.
 
 def index(request):
@@ -11,7 +12,7 @@ def index(request):
 @login_required
 def topics(request):
 
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
 
     context = {
         'topics': topics
@@ -23,6 +24,10 @@ def topics(request):
 def topic(request, topic_id):
 
     topic = Topic.objects.get(id=topic_id)
+
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     
     context = {
@@ -41,7 +46,11 @@ def create(request):
         form = TopicForm(data=request.POST)
 
         if form.is_valid():
-            form.save()
+            
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+
             return redirect('learning_logs:topics')
 
     context = {
@@ -81,6 +90,9 @@ def edit_entry(request, entry_id):
 
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
